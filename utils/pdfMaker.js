ec413da6aa6781ed99976200cc415e242b208fc4
nathanie.assan <nathanie.assan@turntabl.io>
2023-04-21 23:38:30 +0000

    added logs to trace create election error

--- utils/pdfMaker.js
+++ utils/pdfMaker.js
@@ -14,7 +14,7 @@ const createComplexPdf = async (
   var fonts = {
     Roboto: {
       normal: __dirname + "/fonts/roboto/Roboto-Regular.ttf",
-      bold: "fonts/roboto/Roboto-Medium.ttf",
+      bold: __dirname + "/fonts/roboto/Roboto-Regular.ttf",
       italics: "fonts/roboto/Roboto-Italic.ttf",
       bolditalics: "fonts/roboto/Roboto-MediumItalic.ttf",
     },
