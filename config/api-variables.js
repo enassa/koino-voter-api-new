4afaf6f8909cea66909486c4858849ca8568d5a5
nathanie.assan <nathanie.assan@turntabl.io>
2023-05-04 21:46:00 +0000

    updated oauth credentials

--- config/api-variables.js
+++ config/api-variables.js
@@ -1,14 +1,14 @@
 const CREDENTIALS = {
   client_id:
-    "70111791974-j7b2b1snbtuk1h3og7pc5ko1gep2j2km.apps.googleusercontent.com",
+    "70111791974-8id1l4qrvaeg390g1ols5s2qgp0ftqbs.apps.googleusercontent.com",
   project_id: "koinovote-361416",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_secret: "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",
+  client_secret: "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",
   redirect_uris: ["https://developers.google.com/oauthplayground"],
 
   refresh_token:
-    "1//04l0xgZ9q1St5CgYIARAAGAQSNwF-L9IrIf9ShFkfEpEM10-v9cDUHqgzGchfn00DSl1bnZYw_Phfg3i2hKX-XG7YRNXlkxA6uK4",
+    "1//04QF8_Hpn5OkoCgYIARAAGAQSNwF-L9IrEe2SdnKRTABVfEAHFfvh06gOV99gXSPRwE0EuK69iLTWdRJRZFmW_-4DQHHLF3pS5kA",
 };
 module.exports = CREDENTIALS;
